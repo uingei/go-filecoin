d9a817baa6edc0bfbb031b66edd1851b1a0d05e8
shannonwells <shannonwells@users.noreply.github.com>
2019-06-18 17:36:44 -0700

    WIP sketching out fault monitor

--- node/node.go
+++ node/node.go
@@ -94,15 +94,20 @@ type nodeChainSyncer interface {
 	HandleNewTipset(ctx context.Context, tipsetCids types.SortedCidSet) error
 }
 
+type faultMonitor interface {
+	HandleNewTipSet(context.Context, chain.TipsetIterator, types.TipSet) error
+}
+
 // Node represents a full Filecoin node.
 type Node struct {
 	host     host.Host
 	PeerHost host.Host
 
-	Consensus   consensus.Protocol
-	ChainReader nodeChainReader
-	Syncer      nodeChainSyncer
-	PowerTable  consensus.PowerTableView
+	Consensus    consensus.Protocol
+	ChainReader  nodeChainReader
+	Syncer       nodeChainSyncer
+	FaultMonitor faultMonitor
+	PowerTable   consensus.PowerTableView
 
 	BlockMiningAPI *block.MiningAPI
 	PorcelainAPI   *porcelain.API
@@ -432,6 +437,8 @@ func (nc *Config) Build(ctx context.Context) (*Node, error) {
 	msgPool := core.NewMessagePool(nc.Repo.Config().Mpool, consensus.NewIngestionValidator(chainState, nc.Repo.Config().Mpool))
 	inbox := core.NewInbox(msgPool, core.InboxMaxAgeTipsets, chainStore)
 
+	faultMonitor := consensus.FaultMonitor{}
+
 	msgQueue := core.NewMessageQueue()
 	outboxPolicy := core.NewMessageQueuePolicy(chainStore, core.OutboxMaxAgeRounds)
 	msgPublisher := newDefaultMessagePublisher(pubsub.NewPublisher(fsub), net.MessageTopic, msgPool)
@@ -459,6 +466,7 @@ func (nc *Config) Build(ctx context.Context) (*Node, error) {
 		cborStore:    &cstOffline,
 		Consensus:    nodeConsensus,
 		ChainReader:  chainStore,
+		FaultMonitor: faultMonitor,
 		Syncer:       chainSyncer,
 		PowerTable:   powerTable,
 		PorcelainAPI: PorcelainAPI,
@@ -680,7 +688,13 @@ func (node *Node) handleNewHeaviestTipSet(ctx context.Context, head types.TipSet
 					log.Error(err)
 				}
 			}
+
+			err := node.FaultMonitor.HandleNewTipSet(ctx, newHead)
+			if err != nil {
+				log.Error("fault monitoring new block from network", err)
+			}
 			node.HeaviestTipSetHandled()
+
 		case <-ctx.Done():
 			return
 		}
