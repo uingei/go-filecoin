950a052d7bfe9e433c56e056f49c1b6e4ba9fce3
shannonwells <shannonwells@users.noreply.github.com>
2019-06-19 17:40:20 -0700

    interfaces only

--- consensus/fault_monitor.go
+++ consensus/fault_monitor.go
@@ -2,19 +2,22 @@ package consensus
 
 import (
 	"context"
-
-	"github.com/filecoin-project/go-filecoin/chain"
-	"github.com/filecoin-project/go-filecoin/porcelain"
 	"github.com/filecoin-project/go-filecoin/types"
 )
 
+// TSIter is an iterator over a TipSet
+type TSIter interface {
+	Complete() bool
+	Next() error
+	Value() types.TipSet
+}
+
 // FaultMonitor checks each new tipset for faults
 type FaultMonitor struct {
-	porcelain porcelain.API
 }
 
 // HandleNewTipSet receives an iterator over the current chain, and a new tipset
 // and checks the new tipset for fault errors, iteratoring over chnIter
-func (fm *FaultMonitor) HandleNewTipSet(ctx context.Context, iter chain.TipsetIterator, newTs types.TipSet) {
+func (fm *FaultMonitor) HandleNewTipSet(ctx context.Context, iter TSIter, newTs types.TipSet) {
 
 }
